e376fa209af0c6473c7d545b24c35c45377647d8
Danil <134092671+Danildanil228@users.noreply.github.com>
2023-10-30 11:41:11 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,10 @@
-﻿Console.Write("Введите значение от 1 до 3: ");
-int x = int.Parse(Console.ReadLine());
-switch(x)
+int num = 2;
+Console.WriteLine("Четные числа кратные 5 от 2 до 100: ");
+while (num <= 100)
 {
-    case 1:
-        {
-            int a = 10, b = 34, c = 65;
-            if (a>107) Console.Write($"{a}>1101011");
-            if (b > 107) Console.Write($"{b}>1101011");
-            if (c > 107) Console.Write($"{c}>1101011");
-            else Console.Write("Нет чисел которые больше чем 1101011");
-        }
-        break;
-    case 2:
-        {
-            int a = 76, b = 18, c = 55;
-            if (a > 107) Console.Write($"{a}>1101011");
-            if (b > 107) Console.Write($"{b}>1101011");
-            if (c > 107) Console.Write($"{c}>1101011");
-            else Console.Write("Нет чисел которые больше чем 1101011");
-        }
-        break;
-    case 3:
-        {
-            int a = 81, b = 75, c = 20;
-            if (a > 107) Console.Write($"{a}>1101011");
-            if (b > 107) Console.Write($"{b}>1101011");
-            if (c > 107) Console.Write($"{c}>1101011");
-            else Console.Write("Нет чисел которые больше чем 1101011");
-        }
-        break;
+    if (num % 2 == 0 && num % 5 == 0)
+    {
+        Console.WriteLine(num);
+    }
+    num++;
 }
